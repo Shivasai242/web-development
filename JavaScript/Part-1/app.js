d3d4c77675eb0be5f67c76802ee596c43a7b0dcf
Shivasai242 <shivasaigoudbairi@gmail.com>
2025-12-24 20:57:26 +0530

    js

--- JavaScript/Part-1/app.js
+++ JavaScript/Part-1/app.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+//console.log("Hello World!");
+//console.log("Apna College");
+//console.log("Vishnu");
+let a=10;
+let b=5;
+console.log(" sum is :",a+b);
+//this is a comment
+let pencilPrice=10;
+let eraserPrice=5;
+//output="the total price :"+(pencilPrice+eraserPrice)+"Rupees"
+output=`the total price is :${pencilPrice+eraserPrice} Rupees.`; //back ticks
+console.log(output)
