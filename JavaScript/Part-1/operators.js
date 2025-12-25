6055ebd1ea6fc76a689eb12f7a9bc696d0f24c51
Shivasai242 <shivasaigoudbairi@gmail.com>
2025-12-25 20:24:15 +0530

    js

---
+++ JavaScript/Part-1/operators.js
@@ -0,0 +1,19 @@
+//Arithmetic operators
+let a=10;
+let b=5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+//unary
+console.log(a++); //10
+console.log(++a); //12
+
+
+//Assignment
+console.log(a=b);
+console.log(a+=b);
+console.log(a*=b);
